a02547001dcbd5bf93629102e2f9931e0723c995
M1778M <m1778.pc@gmail.com>
2024-12-05 03:23:04 +0330

    Updated setup.py to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-VERSION = '0.1.1'
-DESCRIPTION = "OMEGAPy Python Toolkit all projects need"
-INSTALL_REQUIRES = ["openai","vosk","SpeechRecognition","pyaudio","pydub","openai-whisper","pySmartDL"]
-
-
-setup(name="omglib",
-      version=VERSION,
-      py_modules=['omglib'],
-      description=DESCRIPTION,
-      author="M1778",
-      author_email="m1778.pc@gmail.com")
+setup()
