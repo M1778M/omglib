215d20cb55ed2d5a358192203f5b2453d76caa3f
M1778M <m1778.pc@gmail.com>
2024-12-16 21:43:00 +0330

    UPD

--- omglib/llm/tools.py
+++ omglib/llm/tools.py
@@ -7,7 +7,7 @@ import asyncio
 import serpapi
 from pathlib import Path
 import subprocess
-
+from .tools_base import *
 from ..tools.dicttools import perfectdict
 
 # Built in functions ---------------------------------------------
@@ -196,75 +196,6 @@ def search_web(query:str,search_engine:str='google',country_code:str="US"): # Se
 
 
 
-# Formatation Section...
-class TTArray:
-    def __init__(self,items_type:type):
-        self.items_type=items_type
-    @property
-    def t(self):
-        return self.items_type
-
-def xtype(type_,description:str,enum:list=None):
-    if description == 'no_desc':
-        if type_ == str and enum != None:
-            return {"type":"string","enum":enum}
-        elif type_ == int and enum != None:
-            return {"type":"integer","enum":enum}
-        elif type_ == float and enum != None:
-            return {"type":"float","enum":enum}
-        elif type_ == str:
-            return {"type":"string"}
-        elif type_ == int:
-            return {"type":"integer"}
-        elif type_ == float:
-            return {"type":"float"}
-        elif isinstance(type_,TTArray):
-            return {"type":"array","items":type_.t}
-        else:
-            raise ValueError("Invalid value for xtype.argumentParser")
-    if type_ == str and enum != None:
-        return {"type":"string","enum":enum,"description":description}
-    elif type_ == int and enum != None:
-        return {"type":"integer","enum":enum,"description":description}
-    elif type == float and enum != None:
-        return {"type":"float","enum":enum,"description":description}
-    elif type_ == str:
-        return {"type":"string","description":description}
-    elif type_ == int:
-        return {"type":"integer","description":description}
-    elif type_ == float:
-        return {"type":"float","description":description}
-    elif isinstance(type_,TTArray):
-        return {"type":"array","description":description,"items":type_.t}
-    else:
-        raise ValueError("Invalid value for xtype.argumentParser")
-
-def gen_func(func_name:str,func_desc:str,required=all,**kwargs): # example -> gen_func("insert_members",required=["mname","mbudget"],mname=xtype(str,"member name"),mbudget=xtype(int,"member budget in us dollars",enum=[100,200,300,400]),keywords=xtype(TTArray(xtype(str,"no_desc")),"keyword"))
-    properties = {}
-    for key in kwargs:
-        properties[key] = kwargs[key]
-    
-    return {
-        "type": "function",
-        "function": {
-            "name": func_name,
-            "description":func_desc,
-            "parameters": {
-                "type": "object",
-                "properties": properties,
-                "required": required if required != all else list(kwargs.keys()),
-                "additionalProperties": False,
-            },
-        },
-    }
-def gen_func_response(tool_call_id:str,func_name:str,func_output:json): # out.choices[0].message.tool_calls[n].id
-    return {
-        "tool_call_id":tool_call_id,
-        "role":"tool",
-        "name":func_name,
-        "content":func_output
-    }
-
 
 
 ALL_AVAILABLE_TOOLS = [
@@ -279,9 +210,9 @@ ALL_AVAILABLE_TOOLS = [
     gen_func("OpenPath","simply opens a path in windows explorer, code:`explorer.exe {path}`",path=xtype(str,"The path to open in windows explorer")),
     gen_func("cython_inline","A function that will get an argument as input for cython.inline function inline compiler and returns a output",inline_text=xtype(str,"The inline string to be compiled and executed with cython.inline function.")),
     gen_func("run_cmd","Runs the given command line/shell code using subprocess and returns output",cmd_code=xtype(str,"The cmd code to run")),
-    
+
 ]
 
-def call_func(func_name:str,**func_args):
+def call_func(func_name:str,func_args:dict):
     return globals()[func_name](**func_args)
 
